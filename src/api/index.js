1e07c29ec9f9f7b1bd23f98f7f0f9ed7d6882ce3
ukaran11 <ukaran11@gmail.com>
2021-09-06 00:55:15 +0530

    Utsav | Fetched restaurants based on location

--- src/api/index.js
+++ src/api/index.js
@@ -1,23 +1,22 @@
 import axios from "axios";
 
 const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary';
-const options = {
-  params: {
-    bl_latitude: '11.847676',
-    tr_latitude: '12.838442',
-    bl_longitude: '109.095887',
-    tr_longitude: '109.149359',
-  },
-  headers: {
-    'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
-    'x-rapidapi-key': '43aedcd373msh75ecc7e0896fec7p15ca09jsn15efb283dbc7'
-  }
-};
 
-export const getPlacesData = async () => {
+
+export const getPlacesData = async (sw, ne) => {
     try {
-        const { data: {data } } = await axios.get(URL, options);
-
+        const { data: {data } } = await axios.get(URL, {
+          params: {
+            bl_latitude: sw.lat,
+            tr_latitude: ne.lat,
+            bl_longitude: sw.lng,
+            tr_longitude: ne.lng,
+          },
+          headers: {
+            'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
+            'x-rapidapi-key': '43aedcd373msh75ecc7e0896fec7p15ca09jsn15efb283dbc7'
+          }
+        })
         return data;
     } catch(error) {
         console.log(error);

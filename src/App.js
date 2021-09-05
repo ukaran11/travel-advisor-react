40948056cfe9797df5b56318a9fcbb9f04b38f99
ukaran11 <ukaran11@gmail.com>
2021-09-06 00:31:23 +0530

    Utsav | Got the coords and bounds

--- src/App.js
+++ src/App.js
@@ -9,12 +9,21 @@ import Map from './components/Map/Map';
 const App = () => {
     const [places, setPlaces] = useState([]);
 
+    const [coordinates, setCoordinates] = useState({});
+    const [bounds, setBounds] = useState(null);
+
     useEffect(() => {
-        getPlacesData().then((data) => {
+        navigator.geolocation.getCurrentPosition(({ coords: {latitude, longitude}}) => {
+            setCoordinates({ lat: latitude, lng: longitude});
+        })
+    }, []);
+
+    useEffect(() => {
+        getPlacesData(bounds.sw, bounds.ne).then((data) => {
             console.log(data);
             setPlaces(data);
         })
-    }, []);
+    }, [coordinates, bounds]);
 
     return (
        <>
@@ -25,7 +34,11 @@ const App = () => {
                 <List />
             </Grid>
             <Grid item xs={12} md={8} >
-                <Map />
+                <Map 
+                    setCoordinates = {setCoordinates}
+                    setBounds = {setBounds}
+                    coordinates = {coordinates}
+                />
             </Grid>
         </Grid>
        </>

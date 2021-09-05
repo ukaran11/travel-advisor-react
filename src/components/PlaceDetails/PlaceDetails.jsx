15b9e389c215925529f41ed52778412faa58f078
ukaran11 <ukaran11@gmail.com>
2021-09-06 01:42:34 +0530

    Utsav | Added Cards of places

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -1,8 +1,33 @@
 import React from 'react';
+import { Box, Typography, Button, Card, CardMedia, CardContent, CardActions, Chip } from '@material-ui/core';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import PhoneIcon from '@material-ui/icons/Phone';
+import Rating from '@material-ui/lab/Rating';
+
+import useStyles from './styles';
 
 const PlaceDetails = ({ place }) => {
+    const classes = useStyles();
+
     return (
-        <h1>{place.name}</h1>
+        <Card elevation={6}>
+            <CardMedia 
+                style={{height: 350}}
+                image={place.photo ? place.photo.images.large.url : 'https://www.google.com/url?sa=i&url=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Frestaurant&psig=AOvVaw19R2xEMXpqbl7tGkTc9cfM&ust=1630958620133000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCKCrx7vQ6PICFQAAAAAdAAAAABAH'}
+                title={place.name}
+            />
+            <CardContent>
+                <Typography gutterBottom variant="h5">{place.name}</Typography>
+                <Box display="flex" justifyContent="space-between">
+                    <Typography variant="subtitle1">Price</Typography>
+                    <Typography gutterBottom variant="subtitle1">{place.price_level}</Typography>
+                </Box>
+                <Box display="flex" justifyContent="space-between">
+                    <Typography variant="subtitle1">Ranking</Typography>
+                    <Typography gutterBottom variant="subtitle1">{place.ranking}</Typography>
+                </Box>
+            </CardContent>
+        </Card>
     );
 }
 
